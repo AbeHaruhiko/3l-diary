e2cd8114c01d24e55c888009088c08dd270c5581
AbeHaruhiko <caliconography@gmail.com>
2016-02-12 20:55:43 +0900

    [modify] App.vueをtsに。

---
+++ src/App.ts
@@ -0,0 +1,19 @@
+"use strict"
+
+import VueComponent from "vue-class-component";
+
+import { Hello } from "./components/Hello";
+
+@VueComponent({
+    template: require("./App.html"),
+    components: {
+      Hello
+    }
+})
+export class App {
+    data () {
+        return {
+          msg: 123
+        }
+      }
+}
