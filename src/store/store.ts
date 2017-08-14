a592cba7b506eaaa70f716adf081e84e2d0f7a34
AbeHaruhiko <h.abe@s2soft.co.jp>
2017-08-14 15:34:03 +0900

    consts, store導入

---
+++ src/store/store.ts
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import * as consts from '../consts/consts'
+
+
+Vue.use(Vuex)
+
+
+// Chroneco用Stateの型定義
+interface ChronecoState {
+  message: string
+}
+
+// root state object.
+// each Vuex instance is just a single state tree.
+const state: ChronecoState = {
+  message: null
+}
+
+// mutations are operations that actually mutates the state.
+// each mutation handler gets the entire state tree as the
+// first argument, followed by additional payload arguments.
+// mutations must be synchronous and can be recorded by plugins
+// for debugging purposes.
+const mutations = {
+  setMessage (state, message: string) {
+    state.message = message
+  }
+}
+
+// actions are functions that causes side effects and can involve
+// asynchronous operations.
+const actions = {
+
+}
+
+// getters are functions
+const getters = {
+  getMessage: string => () => state.message
+}
+
+// A Vuex instance is created by combining the state, mutations, actions,
+// and getters.
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations
+})
